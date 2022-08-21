7ad2cbebaa40156be2187ec51a0185b77eeda9af
zemira kehati <kehati10@gmail.com>
2022-08-21 23:38:54 +0300

    member list desined

--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -5,28 +5,55 @@ import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { NewMember } from '../cmps/NewMember';
-import { Button } from '@mui/material';
+import sortIcon from '../assets/icons/sort.svg';
+import filterIcon from '../assets/icons/filter.svg';
 
 import { contactService } from '../services/contactService';
 
 export const Member = () => {
   let memberList = contactService.query();
   return (
-    <div className="Members">
-      <Button>Add new member +</Button>
-      <Button>filter</Button>
-      <Button>sort</Button>
-      <h3>Showing {memberList.length} members</h3>
+    <section className="Members">
+      <div className="left flex space-between align-center gap-1 mar-t-56">
+        <div className="left flex align-center gap-1">
+          <button className="members-add-button fs20 lh-35 br-10">
+            Add new member +
+          </button>
+          <button className="members-filter-btn flex align-center justify-center br-50">
+            <div
+              style={{
+                backgroundImage: `url(${filterIcon})`,
+                width: `20px`,
+                height: `25px`,
+                backgroundSize: `contain`,
+              }}
+            />
+            filter
+          </button>
+          <button className="members-sort-btn flex align-center justify-center br-50">
+            <div
+              style={{
+                backgroundImage: `url(${sortIcon})`,
+                width: `20px`,
+                height: `25px`,
+                backgroundSize: `contain`,
+              }}
+            />
+            sort
+          </button>
+        </div>
+        <span>Showing {memberList.length} members</span>
+      </div>
       <React.Fragment>
-        <Table size="small">
+        <Table size="small" className="mar-t-63">
           <TableHead>
             <TableRow>
               <TableCell></TableCell>
-              <TableCell>full name</TableCell>
-              <TableCell>Phone number</TableCell>
-              <TableCell>Team</TableCell>
-              <TableCell>Start time</TableCell>
-              <TableCell>End time</TableCell>
+              <TableCell className="table-header">Full name</TableCell>
+              <TableCell className="table-header">Phone number</TableCell>
+              <TableCell className="table-header">Team</TableCell>
+              <TableCell className="table-header">Start time</TableCell>
+              <TableCell className="table-header">End time</TableCell>
               <TableCell></TableCell>
             </TableRow>
           </TableHead>
@@ -44,11 +71,11 @@ export const Member = () => {
                     }}
                   />
                 </TableCell>
-                <TableCell>{member.name}</TableCell>
-                <TableCell>{member.phone}</TableCell>
-                <TableCell>{member.team}</TableCell>
-                <TableCell>10:00</TableCell>
-                <TableCell>12:00</TableCell>
+                <TableCell className="table-content">{member.name}</TableCell>
+                <TableCell className="table-content">{member.phone}</TableCell>
+                <TableCell className="table-content">{member.team}</TableCell>
+                <TableCell className="table-content">10:00</TableCell>
+                <TableCell className="table-content">12:00</TableCell>
                 <TableCell>...</TableCell>
               </TableRow>
             ))}
@@ -56,6 +83,6 @@ export const Member = () => {
         </Table>
       </React.Fragment>
       {/* <NewMember></NewMember> */}
-    </div>
+    </section>
   );
 };
