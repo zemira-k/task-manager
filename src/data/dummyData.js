ee10e3b1a9c7041ba263a4deca26acbf46b5a002
Aviv <aviv80010@gmail.com>
2022-08-21 15:25:08 +0300

    worked on header, added current page title, time, colors..

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -6,29 +6,14 @@ export const AppInfo = {
 }
 export const Task = [
     {
-        _id : "t0001",
+        _id: "t0001",
         title: "Plan tactic to players",
-        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae qui aperiam maxime hic fugiat a magnam, corrupti neque iusto tenetur nesciunt impedit sit nisi quam reprehenderit cum distinctio optio Dolor",
-        time : new Date(),
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae qui aperiam maxime hic fugiat a magnam, corrupti neque iusto tenetur nesciunt impedit sit nisi quam reprehenderit cum distinctio optio Dolor",
+        time: new Date(),
         priority: 3,
-        done : false
+        done: false
     },
-    {
-        _id : "t0002",
-        title: "Plan traning to Goalkeeper",
-        desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae qui aperiam maxime hic fugiat a magna",
-        time : new Date(),
-        priority: 2,
-        done : false
-    },
-    {
-        _id : "t0003",
-        title: "Plan traning to Defence players",
-        desc : "Lorem ipsum dolor sit ametr",
-        time : new Date(),
-        priority: 1,
-        done : true
-    },
+
 ]
 export const Contact = [
     {
