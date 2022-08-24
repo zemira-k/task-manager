e1a6a95c4f22dfcb6ddd56cc3423a811040e0cfa
zemira kehati <kehati10@gmail.com>
2022-08-24 15:54:49 +0300

    overview desined

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,23 +1,59 @@
-import { Workers } from '../cmps/Workers';
 import { Statistics } from '../cmps/Statistics';
 import { DailyTasks } from '../cmps/Daily-tasks';
+import { Workers } from '../cmps/Workers';
 import { Comments } from '../cmps/Comments';
+import statisticsIcon from '../assets/icons/statistics.svg';
+import dailyTasksIcon from '../assets/icons/daily-tasks.svg';
+import workerIcon from '../assets/icons/workers.svg';
+import commentsIcon from '../assets/icons/comments.svg';
 
 export const Overview = () => {
+  const overviewTitles = [
+    {
+      component: <Statistics />,
+      class: 'item1',
+      title: 'Statistics',
+      icon: statisticsIcon,
+    },
+    {
+      component: <DailyTasks />,
+      class: 'item2',
+      title: 'Daily-tasks',
+      icon: dailyTasksIcon,
+    },
+    {
+      component: <Workers />,
+      class: 'item3',
+      title: 'Workers',
+      icon: workerIcon,
+    },
+    {
+      component: <Comments />,
+      class: 'item4',
+      title: 'Comments',
+      icon: commentsIcon,
+    },
+  ];
+
   return (
     <div className="grid-container">
-      <div className="item1">
-        <Statistics />
-      </div>
-      <div className="item2">
-        <DailyTasks />
-      </div>
-      <div className="item3">
-        <Workers />
-      </div>
-      <div className="item4">
-        <Comments />
-      </div>
+      {overviewTitles.map((title, i) => (
+        <div key={i} className={title.class}>
+          <div className="flex title-container">
+            <div
+              style={{
+                backgroundImage: `url(${title.icon})`,
+                width: `16px`,
+                height: `16px`,
+                backgroundSize: `contain`,
+                backgroundRepeat: `no-repeat`,
+              }}
+            />
+            <p className="overview-titles">{title.title}</p>
+          </div>
+          {title.component}
+        </div>
+      ))}
     </div>
   );
 };
