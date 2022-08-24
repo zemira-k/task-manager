b136629dd22c360e53ae9725e0ab867fabf5650b
Aviv <aviv80010@gmail.com>
2022-08-24 16:47:49 +0300

    added status toggle function for task

--- src/cmps/TaskTable.jsx
+++ src/cmps/TaskTable.jsx
@@ -6,15 +6,18 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Checkbox } from '@mui/material';
 
 const fields = ['Task', 'Teams', 'Members']
 
-export const TaskTable = ({ tasks }) => {
+export const TaskTable = ({ tasks, onToggleTaskStatus }) => {
+
     return (
         <TableContainer sx={{ padding: "1rem" }} component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
+                        <TableCell><Checkbox /></TableCell>
                         <TableCell>Times</TableCell>
                         {
                             fields.map(field =>
@@ -27,19 +30,32 @@ export const TaskTable = ({ tasks }) => {
                     {tasks.map((task) => {
                         const { title, teams, members } = task
                         const fieldsToDisplay = [title, JSON.stringify(teams), JSON.stringify(members)]
+                        const checked = task.status === 'done'
                         return <TableRow
                             key={task._id}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                         >
+
+                            <TableCell component="th" scope="row">
+                                <Checkbox onChange={() => {
+                                    task.status = task.status === 'done' ? 'todo' : 'done'
+                                    onToggleTaskStatus(task)
+                                }} checked={checked} />
+                            </TableCell>
+
                             <TableCell component="th" scope="row">
                                 {task.time}
                             </TableCell>
                             {fieldsToDisplay.map(field => <TableCell key={field} align="right">{field}</TableCell>)}
+                            <TableCell align="right">
+                                <button className="btn-task-actions">Edit</button>
+                            </TableCell>
+
                         </TableRow>
                     }
                     )}
                 </TableBody>
             </Table>
-        </TableContainer>
-    );
+        </TableContainer >
+    )
 }
--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -4,12 +4,21 @@ import { taskService } from "../services/taskService"
 
 export const Task = () => {
   const [tasks, setTasks] = useState(taskService.query())
+
+  function onToggleTaskStatus(taskToUpdate) {
+    taskService.update(taskToUpdate)
+    setTasks(prevTasks => prevTasks.map(task => {
+      if (task._id === taskToUpdate._id) return taskToUpdate
+      return task
+    }))
+  }
+
   if (!tasks) return <div>loading..</div>
   return (
     <section className="task-page main-page pad-1">
       <section className="actions">
       </section>
-      <TaskTable tasks={tasks} />
+      <TaskTable tasks={tasks} onToggleTaskStatus={onToggleTaskStatus} />
     </section>
   )
 }
