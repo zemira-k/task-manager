edc3a919f2e9e5559bb3dcac2c844ae92aa05141
zemira kehati <kehati10@gmail.com>
2022-08-30 00:16:25 +0300

    added comments to overview

--- src/cmps/Comments.jsx
+++ src/cmps/Comments.jsx
@@ -1,3 +1,31 @@
-export const Comments = () => {
-  return <div className="comments"></div>;
+import { Popup } from '../cmps/Popup';
+import avatar from '../assets/imgs/demo-members/1.jpg';
+
+export const Comments = props => {
+  let { isOpen, onClose, title } = props;
+  return (
+    <div className="comments">
+      <div className="flex space-between align-center">
+        <div className="flex align-center">
+          <div
+            className="comments-avatar"
+            style={{
+              backgroundImage: `url(${avatar})`,
+            }}
+          />
+          <p>Emptying rubbish and bins</p>
+        </div>
+        <p>44m ago</p>
+      </div>
+      {isOpen && (
+        <Popup
+          formWidth="749px"
+          isOpen={isOpen}
+          onClose={onClose}
+          title={title}
+          saveButtonVisible={false}
+        ></Popup>
+      )}
+    </div>
+  );
 };
