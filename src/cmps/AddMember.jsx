3c9efa1b65273d46125ddd51f2e7d8c374866636
zemira kehati <kehati10@gmail.com>
2022-08-30 00:17:48 +0300

    edit add member component to open with main popup

--- src/cmps/AddMember.jsx
+++ src/cmps/AddMember.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { memberService } from '../services/memberService';
+import { Popup } from './Popup';
 import search from '../assets/icons/search.svg';
 
 export const AddMember = ({
@@ -69,97 +70,89 @@ export const AddMember = ({
   };
 
   return (
-    <div className={`popup ${isOpen ? 'popup-open' : ''}`}>
-      <div className="add-member">
-        <form
-          className="flex column align-center justify-center"
-          onSubmit={name ? hadleEditSubmit : hadleAddSubmit}
-        >
-          <button
-            onClick={handleCloseAddMemberClick}
-            className="popup-close clean-btn"
-          ></button>
-          <p className="form-title">Member profile</p>
-          <div
-            className="avatar"
-            style={{
-              backgroundImage: `url(${avatar})`,
-            }}
-          ></div>
-          <div>
-            <p className="form-subtitle">Personal Settings</p>
-            <div className="flex gap-25">
-              {formInput1.map(input => (
-                <div className="flex column">
-                  <label className="form-label">{input.labelName}</label>
-                  <input
-                    className="form-input"
-                    name={input.inputName}
-                    type={input.type}
-                    defaultValue={input.defaultValue}
-                    onChange={handleChange}
-                  ></input>
-                </div>
-              ))}
+    <Popup
+      formWidth="749px"
+      isOpen={isOpen}
+      title="Member profile"
+      onClose={handleCloseAddMemberClick}
+      saveButtonVisible={true}
+      onSubmit={name ? hadleEditSubmit : hadleAddSubmit}
+    >
+      <div
+        className="avatar"
+        style={{
+          backgroundImage: `url(${avatar})`,
+        }}
+      ></div>
+      <div>
+        <p className="form-subtitle">Personal Settings</p>
+        <div className="flex gap-25">
+          {formInput1.map(input => (
+            <div className="flex column">
+              <label className="form-label">{input.labelName}</label>
+              <input
+                className="form-input"
+                name={input.inputName}
+                type={input.type}
+                defaultValue={input.defaultValue}
+                onChange={handleChange}
+              ></input>
             </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <p className="form-subtitle">Employee Settings</p>
+        <div className="flex gap-25">
+          <div className="flex column">
+            <label className="form-label">Employee No.*</label>
+            <input
+              className="form-input"
+              name="officialID"
+              defaultValue={officialID}
+              onChange={handleChange}
+            />
           </div>
-          <div>
-            <p className="form-subtitle">Employee Settings</p>
-            <div className="flex gap-25">
-              <div className="flex column">
-                <label className="form-label">Employee No.*</label>
-                <input
-                  className="form-input"
-                  name="officialID"
-                  defaultValue={officialID}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="flex column">
-                <label className="form-label">Role*</label>
-                <input
-                  className="form-input"
-                  name="role"
-                  defaultValue={role}
-                  onChange={handleChange}
-                ></input>
-              </div>
-              <div className="flex column">
-                <label className="form-label">Team*</label>
-                <div
-                  className="flex align-center"
-                  style={{ width: `192px`, height: `55px` }}
-                >
+          <div className="flex column">
+            <label className="form-label">Role*</label>
+            <input
+              className="form-input"
+              name="role"
+              defaultValue={role}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div className="flex column">
+            <label className="form-label">Team*</label>
+            <div
+              className="flex align-center"
+              style={{ width: `192px`, height: `55px` }}
+            >
+              <div
+                className="flex align-center space-between"
+                style={{ width: `166px` }}
+              >
+                <button className="search-btn clean-btn flex justify-center">
                   <div
-                    className="flex align-center space-between"
-                    style={{ width: `166px` }}
-                  >
-                    <button className="search-btn clean-btn flex justify-center">
-                      <div
-                        className="search-icon"
-                        style={{
-                          backgroundImage: `url(${search})`,
-                        }}
-                      ></div>
-                      <p className="search-text">search</p>
-                    </button>
-                    <button className="add-btn clean-btn">+ add</button>
-                  </div>
-                </div>
-                {/* <input
+                    className="search-icon"
+                    style={{
+                      backgroundImage: `url(${search})`,
+                    }}
+                  ></div>
+                  <p className="search-text">search</p>
+                </button>
+                <button className="add-btn clean-btn">+ add</button>
+              </div>
+            </div>
+            {/* <input
                   className="form-input"
                   name="team.title"
                   defaultValue={team}
                   onChange={handleChange}
                 ></input> */}
-              </div>
-            </div>
           </div>
-          <button className="save-btn br-10" type="submit">
-            Save
-          </button>
-        </form>
+        </div>
       </div>
-    </div>
+    </Popup>
   );
 };
