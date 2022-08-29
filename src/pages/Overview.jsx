850d78356aad63cf9f38174186e494d2c6fd69ae
zemira kehati <kehati10@gmail.com>
2022-08-30 00:14:40 +0300

    added states for popups and popup icon

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,59 +1,101 @@
+import React, { useState } from 'react';
 import { Statistics } from '../cmps/Statistics';
 import { DailyTasks } from '../cmps/Daily-tasks';
-import { Workers } from '../cmps/Workers';
+import { WorkersPreview } from '../cmps/WorkersPreview';
 import { Comments } from '../cmps/Comments';
 import statisticsIcon from '../assets/icons/statistics.svg';
 import dailyTasksIcon from '../assets/icons/daily-tasks.svg';
+import popupIcon from '../assets/icons/popup.svg';
 import workerIcon from '../assets/icons/workers.svg';
 import commentsIcon from '../assets/icons/comments.svg';
 
 export const Overview = () => {
+  const [isWorkersPopupOpen, setIsWorkersPopupOpen] = useState(false);
+  const [isCommentsPopupOpen, setIsCommentsPopupOpen] = useState(false);
+  const handlePopupClick = e => {
+    handleCloseClick();
+    e.target.name === 'Workers'
+      ? setIsWorkersPopupOpen(true)
+      : setIsCommentsPopupOpen(true);
+  };
+
+  const handleCloseClick = e => {
+    setIsWorkersPopupOpen(false);
+    setIsCommentsPopupOpen(false);
+  };
   const overviewTitles = [
     {
       component: <Statistics />,
       class: 'item1',
       title: 'Statistics',
       icon: statisticsIcon,
+      popupIcon: false,
     },
     {
       component: <DailyTasks />,
       class: 'item2',
       title: 'Daily-tasks',
       icon: dailyTasksIcon,
+      popupIcon: false,
     },
     {
-      component: <Workers />,
+      component: (
+        <WorkersPreview
+          isOpen={isWorkersPopupOpen}
+          onClose={handleCloseClick}
+          title={'Workers'}
+        />
+      ),
       class: 'item3',
       title: 'Workers',
       icon: workerIcon,
+      popupIcon: true,
     },
     {
-      component: <Comments />,
+      component: (
+        <Comments
+          isOpen={isCommentsPopupOpen}
+          onClose={handleCloseClick}
+          title={'Comments'}
+        />
+      ),
       class: 'item4',
       title: 'Comments',
       icon: commentsIcon,
+      popupIcon: true,
     },
   ];
 
   return (
-    <div className="grid-container">
-      {overviewTitles.map((title, i) => (
-        <div key={i} className={title.class}>
-          <div className="flex title-container">
-            <div
-              style={{
-                backgroundImage: `url(${title.icon})`,
-                width: `1rem`,
-                height: `1rem`,
-                backgroundSize: `contain`,
-                backgroundRepeat: `no-repeat`,
-              }}
-            />
-            <p className="overview-titles">{title.title}</p>
+    <div>
+      <div className="grid-container">
+        {overviewTitles.map((title, i) => (
+          <div key={i} className={`${title.class} flex column`}>
+            <div className="flex align-center space-between w-91 center-self">
+              <div className="flex title-container">
+                <div
+                  className="title-icon"
+                  style={{
+                    backgroundImage: `url(${title.icon})`,
+                  }}
+                />
+                <p className="overview-titles">{title.title}</p>
+              </div>
+              {title.popupIcon && (
+                <button
+                  className="popup-icon clean-btn"
+                  name={title.title}
+                  style={{
+                    backgroundImage: `url(${popupIcon})`,
+                  }}
+                  onClick={handlePopupClick}
+                />
+              )}
+            </div>
+            {title.component}
           </div>
-          {title.component}
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
