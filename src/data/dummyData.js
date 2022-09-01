6c20b3f7ec2a5f4aec6aaff99d4cf298e4eef103
zemira kehati <kehati10@gmail.com>
2022-09-01 15:47:00 +0300

    add data

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -6,27 +6,47 @@ export const Task = [
     title: 'Emptying rubbish and bins',
     time: {
       from: `${new Date().getHours()}:00`,
-      to: `${new Date().getHours() + 1}:00`
+      to: `${new Date().getHours() + 1}:00`,
     }, // When does the task need to be done
 
-    teams: [
-      {
-        _id: '21',
-        name: 'Cleaning',
-        color: '#08C7E0',
-      },
-    ],
+    teams: [{ _id: '01', title: 'Cleaning', color: '#08C7E0' }],
     members: [
       {
         _id: '12',
         fullname: 'Sara omami',
         avatar: Avatar1,
-        email: 'Sara567@gmail.com',
-        phone: '051-856-753',
         role: 'Cleaner',
-        memberNum: 567,
+      },
+      {
+        _id: 'yf58t',
+        fullname: 'Victor Jones',
+        avatar: 'https://randomuser.me/api/portraits/men/65.jpg',
+        role: 'Cleaner',
       },
     ],
+    comments: 1,
+    createdAt: new Date(Date.now() - 60 * 1000 ** 2 * 24), // Task creation date
+    status: 'todo', // todo/done
+  },
+  {
+    _id: '124',
+    title: 'Making dinner for tonight',
+    time: {
+      from: `${new Date().getHours()}:00`,
+      to: `${new Date().getHours() + 1}:00`,
+    }, // When does the task need to be done
+
+    teams: [{ _id: '02', title: 'Kitchen', color: '#FD838F' }],
+    members: [
+      {
+        _id: 'ds32a',
+        fullname: 'barak bachar',
+        avatar: 'https://randomuser.me/api/portraits/women/61.jpg',
+        role: 'chef',
+      },
+    ],
+    comments: 3,
+
     createdAt: new Date(Date.now() - 60 * 1000 ** 2 * 24), // Task creation date
     status: 'todo', // todo/done
   },
@@ -71,7 +91,7 @@ export const Member = [
     avatar: 'https://randomuser.me/api/portraits/women/61.jpg',
     phone: '0525342343',
     mail: 'barak@gmail.com',
-    role: 'coach',
+    role: 'chef',
     officialID: '125487369',
     team: {
       _id: '02',
@@ -117,10 +137,66 @@ export const Member = [
     startTime: '08:10:28',
     endTime: '15:10:50',
   },
+  {
+    _id: 'yf58t',
+    name: 'Victor Jones',
+    avatar: 'https://randomuser.me/api/portraits/men/65.jpg',
+    phone: '0525341343',
+    mail: 'stacy@gmail.com',
+    role: 'cleaner',
+    officialID: '125436369',
+    team: { _id: '02', title: 'Kitchen', color: '#FD838F' },
+    startTime: '08:10:28',
+    endTime: '15:10:50',
+  },
 ];
 
 export const Team = [
   { _id: '01', title: 'Cleaning', color: '#08C7E0' },
   { _id: '02', title: 'Kitchen', color: '#FD838F' },
   { _id: '03', title: 'Childcare', color: '#FFAD7A' },
+];
+
+export const Comments = [
+  {
+    _id: '01',
+    task: 'Emptying rubbish and bins',
+    outComment: [
+      {
+        name: 'Sara Omami',
+        avatar: Avatar1,
+        text: 'Where are the new socks?',
+        createdAt: new Date(Date.now() - 60 * 1000 ** 2 * 24), //comment creation time
+        // need calculate how much time ago
+      },
+    ],
+    inComment: [
+      {
+        name: '',
+        avatar: '',
+        text: '',
+        createdAt: new Date(Date.now() - 60 * 1000 ** 2 * 24),
+      },
+    ],
+  },
+  {
+    _id: '02',
+    task: 'Laundry, ironing and wardrobe',
+    outComment: [
+      {
+        name: 'Victor Jones',
+        avatar: 'https://randomuser.me/api/portraits/men/65.jpg',
+        text: 'Can I get help here?',
+        createdAt: new Date(Date.now() - 60 * 1000 ** 2 * 24),
+      },
+    ],
+    inComment: [
+      {
+        name: '',
+        avatar: '',
+        text: '',
+        createdAt: new Date(Date.now() - 60 * 1000 ** 2 * 24),
+      },
+    ],
+  },
 ];
